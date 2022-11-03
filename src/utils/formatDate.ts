2d41ffe6e344fcfab536ca116bc0d873d135e0d4
Rafael Souza <rafaeljosegomes25@gmail.com>
2022-11-03 20:44:59 -0300

    remove "cerca de" string of date

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -2,9 +2,14 @@ import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 
 export function getDistanceOfDateToNow(date: Date) {
-  const formatedDate = formatDistanceToNow(date, {
-    locale: ptBR
+  let formatedDate = formatDistanceToNow(date, {
+    locale: ptBR,
+    addSuffix: false
   })
+
+  if (formatedDate.includes('cerca de')) {
+    formatedDate = formatedDate.slice(8)
+  }
 
   return `Há ${formatedDate}`
 }
